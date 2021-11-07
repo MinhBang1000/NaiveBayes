2af75f7d389761d423b4f2449493808004632fc0
MinhBang1000 <bradonleminhbang1@gmail.com>
2021-11-07 15:03:12 +0700

    Du lieu lien tuc va phan lop

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,3 +1,6 @@
+import math
+
+
 class MyNavieBayes:
     def __init__(self) -> None:
         pass
@@ -39,43 +42,69 @@ class MyNavieBayes:
             tmp1 = {}
             dic_tt = {}
             dic_tmp1 = {}
-            for k in range(0, len(giatrithuoctinh)):    # Cộng dồn số lượng các giá trị của thuộc tính theo nhãn vào từ điển con
-                dic_tt[giatrithuoctinh[k]] = {}
-                dic_tmp1[giatrithuoctinh[k]] = {}
-                tmp = {}
-                tmp1 = {}
-                for h in range(0, len(giatrinhan)):
-                    tmp[giatrinhan[h]] = 0
-                    tmp1[giatrinhan[h]] = 0
-                dic_tt[giatrithuoctinh[k]] = tmp
-                dic_tmp1[giatrithuoctinh[k]] = tmp1
+            if (self.check_continue(thuoctinh)==True): #Check xem là rời rạc hay liên tục
+                for k in range(0, len(giatrithuoctinh)):    # Cộng dồn số lượng các giá trị của thuộc tính theo nhãn vào từ điển con
+                    dic_tt[giatrithuoctinh[k]] = {}
+                    dic_tmp1[giatrithuoctinh[k]] = {}
+                    tmp = {}
+                    tmp1 = {}
+                    for h in range(0, len(giatrinhan)):
+                        tmp[giatrinhan[h]] = 0
+                        tmp1[giatrinhan[h]] = 0
+                    dic_tt[giatrithuoctinh[k]] = tmp
+                    dic_tmp1[giatrithuoctinh[k]] = tmp1
 
-            # Phải có dấu xuống dòng tại hai dòng for riêng biệt
-            for i in range(0, len(giatrithuoctinh)):
-                for j in range(0, len(thuoctinh)):
-                    if (thuoctinh[j] == giatrithuoctinh[i]):
-                        dic_tt[thuoctinh[j]][nhan[j]] += 1
-                        dic_tmp1[thuoctinh[j]][nhan[j]] += 1
-            
-            for j in range(0,len(giatrinhan)): # Nếu biết có một thuộc tính trong một giá trị bằng 0 thì sẽ quay lại thuộc tính đầu tiên của giá trị nhãn đó để tính lại theo Laplace
-                check_zero = False # Để check giá trị thuộc tính bằng 0
-                check_back = False
-                i=0
-                while (i<len(giatrithuoctinh)):
-                    if (dic_tt[giatrithuoctinh[i]][giatrinhan[j]]!=0 and check_zero==False):
-                        dic_tt[giatrithuoctinh[i]][giatrinhan[j]] = dic_tt[giatrithuoctinh[i]][giatrinhan[j]]/dic_nhan[giatrinhan[j]]
-                    else:
-                        check_zero = True # Để làm Laplace cho tất cả các giá trị của thuộc tính đó
-                        if (check_back==False):
-                            i = 0 # Quay lại cái đầu nếu đang ở các giá trị sau của thuộc tính này
-                            check_back = True # Chỉ set i bằng 0 một lần
-                    if (check_zero==True): # Xử lý Laplace
-                        # print("Gia tri thuoc tinh = ",giatrithuoctinh[i])
-                        # print("Dem duoc = ",dic_tmp1[giatrithuoctinh[i]][giatrinhan[j]])
-                        # print("Bao nhieu nhan nay = ",1/(len(dic_tt)))
-                        # print("Laplace = ",Laplace)
-                        dic_tt[giatrithuoctinh[i]][giatrinhan[j]] = (dic_tmp1[giatrithuoctinh[i]][giatrinhan[j]] + (1/(len(dic_tt)))*Laplace)/(dic_nhan[giatrinhan[j]]+Laplace)
-                    i+=1
+                # Phải có dấu xuống dòng tại hai dòng for riêng biệt
+                for i in range(0, len(giatrithuoctinh)):
+                    for j in range(0, len(thuoctinh)):
+                        if (thuoctinh[j] == giatrithuoctinh[i]):
+                            dic_tt[thuoctinh[j]][nhan[j]] += 1
+                            dic_tmp1[thuoctinh[j]][nhan[j]] += 1
+                
+                for j in range(0,len(giatrinhan)): # Nếu biết có một thuộc tính trong một giá trị bằng 0 thì sẽ quay lại thuộc tính đầu tiên của giá trị nhãn đó để tính lại theo Laplace
+                    check_zero = False # Để check giá trị thuộc tính bằng 0
+                    check_back = False
+                    i=0
+                    while (i<len(giatrithuoctinh)):
+                        if (dic_tt[giatrithuoctinh[i]][giatrinhan[j]]!=0 and check_zero==False):
+                            dic_tt[giatrithuoctinh[i]][giatrinhan[j]] = dic_tt[giatrithuoctinh[i]][giatrinhan[j]]/dic_nhan[giatrinhan[j]]
+                        else:
+                            check_zero = True # Để làm Laplace cho tất cả các giá trị của thuộc tính đó
+                            if (check_back==False):
+                                i = 0 # Quay lại cái đầu nếu đang ở các giá trị sau của thuộc tính này
+                                check_back = True # Chỉ set i bằng 0 một lần
+                        if (check_zero==True): # Xử lý Laplace
+                            # print("Gia tri thuoc tinh = ",giatrithuoctinh[i])
+                            # print("Dem duoc = ",dic_tmp1[giatrithuoctinh[i]][giatrinhan[j]])
+                            # print("Bao nhieu nhan nay = ",1/(len(dic_tt)))
+                            # print("Laplace = ",Laplace)
+                            dic_tt[giatrithuoctinh[i]][giatrinhan[j]] = (dic_tmp1[giatrithuoctinh[i]][giatrinhan[j]] + (1/(len(dic_tt)))*Laplace)/(dic_nhan[giatrinhan[j]]+Laplace)
+                        i+=1
+            else: # Kiểu liên tục
+                X_lt = thuoctinh # Một cột thuộc tính
+                Y_lt = giatrinhan
+                import math
+                for kiemtra_lt in range(0,len(giatrinhan)):
+                    tong_theo_nhan = 0
+                    so_luong = 0
+                    for lt_tt in range(0,len(X_lt)):
+                        if (giatrinhan[kiemtra_lt]==Y_train.iloc[lt_tt]): # Đếm số lượng để tính
+                            tong_theo_nhan = tong_theo_nhan + X_lt.iloc[lt_tt]
+                            so_luong = so_luong + 1
+                    u = tong_theo_nhan/so_luong
+                    o2 = 0
+                    for lt in range(0,len(X_lt)):
+                        if (giatrinhan[kiemtra_lt]==Y_train.iloc[lt]):
+                            o2 = o2+(X_lt.iloc[lt]-u)*(X_lt.iloc[lt]-u)
+                    o2 = o2*(1/(so_luong-1))
+                    o = math.sqrt(o2)
+                    tmp[giatrinhan[kiemtra_lt]] = {
+                        "u":u,
+                        "o2":o2,
+                        "o":o
+                    }
+                    # print("Gia tri nhan: ",giatrinhan[kiemtra_lt])
+                dic_tt["X"] = tmp # Thêm X để đồng bộ với từ điển ở trên
 
             dic_total[X_train.columns[tt_index]] = dic_tt
         
@@ -87,6 +116,7 @@ class MyNavieBayes:
         self.giatrinhan = Y_train.unique()
     
     def predict(self, X_test):
+        import math
         ret_arr = []
         for k in range(0,len(X_test)): # Duyệt qua từng dòng
             X = X_test.iloc[k,:] # Chỉ cần thay đổi chỗ này để cho nhiều dòng
@@ -95,9 +125,24 @@ class MyNavieBayes:
             for i in range(0,len(self.giatrinhan)): # Kiểm tra là Yes hay No trên từng dòng
                 xacsuat = self.dic_label[self.giatrinhan[i]]
                 for j in range(0,len(X)):
-                    xacsuat = xacsuat*self.dic_main[X_test.columns[j]][X.iloc[j]][self.giatrinhan[i]]
+                    check_lien_tuc = False # True la thuoc tinh lien tuc
+                    for key in self.dic_main[X_test.columns[j]].keys():
+                        if (key=="X"):
+                            check_lien_tuc = True
+                            break
+                    if (check_lien_tuc==True): # Kiểu dữ liệu cuả thuộc tính là liên tục
+                        tmp_dic = self.dic_main[X_test.columns[j]]["X"][self.giatrinhan[i]] # Từ điển này lưu u, o2, o
+                        gx = -(X.iloc[j]-tmp_dic["u"])*(X.iloc[j]-tmp_dic["u"])
+                        ex = 2*tmp_dic["o2"]
+                        f = (1/math.sqrt(2*math.pi)*tmp_dic["o"])*math.pow(math.e,gx/ex)
+                        xacsuat = xacsuat*f
+                    else:
+                        xacsuat = xacsuat*self.dic_main[X_test.columns[j]][X.iloc[j]][self.giatrinhan[i]]
                 if (xacsuat>=max_xacsuat): # lựa chọn nhãn
                     max_xacsuat = xacsuat
                     ten_nhan = self.giatrinhan[i]
             ret_arr.append(ten_nhan)
         return ret_arr
+    
+    def show(self):
+        print(self.dic_main)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from math import sqrt
 import pandas as pd
 import numpy as np
 dt = pd.read_csv("play_tennis.csv")
@@ -116,36 +117,94 @@ Y_test = dt_pr_te.Risk_Flag.iloc[84000:]
 # Dùng hàm check_number_or_string để check xem là số hay chữ
 # Dùng len(unique để check xem là rời rạc hay liên tục)
 
-def check_number_or_string(val):
-    try:
-        test = int(val)
-        print("Val is number")
-    except:
-        try:
-            test = float(val)
-            print("Val is number")
-        except:
-            print("Val is string")
+# def check_number_or_string(val):
+#     try:
+#         test = int(val)
+#         print("Val is number")
+#     except:
+#         try:
+#             test = float(val)
+#             print("Val is number")
+#         except:
+#             print("Val is string")
 
-def check_continue(col): # Đưa vào một thuộc tính, một cột
-    check = False # Là dạng liên tục, True là rời rạc
-    for i in range(0,len(col)):
-        check_1 = True
-        try:
-            test = int(col[i])
-            check_1 = False
-        except:
-            try:
-                test = float(col[i])
-                check_1 = False
-            except:
-                check_1 = True
-        if (check_1 == True): # Nếu có tìm thấy một chuỗi thì đây là dạng rời rạc
-            check = check_1
-            break # Đi đến điều kiện số 2 để xem đây có phải rời rạc hay không
+# def check_continue(col): # Đưa vào một thuộc tính, một cột
+#     check = False # Là dạng liên tục, True là rời rạc
+#     for i in range(0,len(col)):
+#         check_1 = True
+#         try:
+#             test = int(col[i])
+#             check_1 = False
+#         except:
+#             try:
+#                 test = float(col[i])
+#                 check_1 = False
+#             except:
+#                 check_1 = True
+#         if (check_1 == True): # Nếu có tìm thấy một chuỗi thì đây là dạng rời rạc
+#             check = check_1
+#             break # Đi đến điều kiện số 2 để xem đây có phải rời rạc hay không
         
-    if (len(col.unique())<=10 and check==False): # Chỉ khi nó không có cái nào là chữ và có ít hơn 10 giá trị khác nhau thì nó là kiểu rời rạc
-        check = True
-    return check
+#     if (len(col.unique())<=10 and check==False): # Chỉ khi nó không có cái nào là chữ và có ít hơn 10 giá trị khác nhau thì nó là kiểu rời rạc
+#         check = True
+#     return check
 
-check_continue(X_train.iloc[:,0])
+# check_continue(X_train.iloc[:,0])
+import math
+X_lt = X_train.iloc[:,0] # Một cột thuộc tính
+Y_lt = Y_train.unique()
+u = X_lt.mean() # Giá trị trung bình
+o2 = (1/len(X_lt))
+for lt in range(0,len(X_lt)):
+    o2 = o2*(X_lt.iloc[lt]-u)*(X_lt.iloc[lt]-u)
+
+# Hàm tính các thông số 
+e = math.sqrt(o2) # Độ lệch chuẩn
+X = pd.DataFrame([1,2,3,4,5,6,7,8,9])
+def lientuc(X_lt,Y_lt):
+    import math
+    u = X_lt.mean() # Giá trị trung bình
+    o2 = 0
+    for lt in range(0,len(X_lt)):
+        o2 = o2+(X_lt.iloc[lt]-u)*(X_lt.iloc[lt]-u)
+    o2 = o2*(1/(len(X_lt)-1))
+    e = math.sqrt(o2) # Độ lệch chuẩn
+    return e
+
+lientuc(X,Y_train.unique())
+#===================================================================
+from NaiveBayes import MyNavieBayes
+import pandas as pd
+import numpy as np
+from sklearn.metrics import accuracy_score
+# Tiền xử lý
+dt_pr = pd.read_csv("Training Data.csv",delimiter=",").iloc[:,1:13]
+X_train = dt_pr.iloc[0:84000,0:11] # Học 84000 mẫu, Test 6000 mẫu
+Y_train = dt_pr.Risk_Flag.iloc[0:84000]
+dt_pr_te = dt_pr                        
+X_test = dt_pr_te.iloc[84000:90000,0:11]
+Y_test = dt_pr_te.Risk_Flag.iloc[84000:90000]
+
+# Training và Test
+nv = MyNavieBayes()
+nv.fit(X_train=X_train,Y_train=Y_train,Laplace=1)
+Y_pre = nv.predict(X_test)
+nv.show()
+print(accuracy_score(Y_test,Y_pre)*100,"%")
+#====================================================================
+# dic1 = {
+#     "a":1,
+#     "b":{
+#         "b1":1,
+#         "b2":2
+#     }
+# }
+# X = ""
+# for i in dic1["b"].keys():
+#     X = i
+#     if (X=="b1"):
+#         break
+X_train = dt.iloc[0:5001,0:4]
+Y_train = dt.Risk_Flag.iloc[0:5001]
+X_test = dt.iloc[5001:5004,0:4]
+Y_test = dt.Risk_Flag.iloc[5001:5004]
